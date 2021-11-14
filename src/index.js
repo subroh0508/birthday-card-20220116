32f02d81a984b4bc9909c67a0a048216b6d2bf41
subroh0508 <subroh.0508@gmail.com>
2021-11-15 02:18:05 +0900

    [edit]Show circles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+import { select } from 'd3-selection';
+
 window.onload = function () {
-  document.getElementsByClassName('container')[0].innerHTML = 'Hello, World!'
-}
+  const circles = [
+    { type: 'small', r: 50, x: 100, y: 150 },
+    { type: 'medium', r: 100, x: 200, y: 150 },
+    { type: 'large', r: 150, x: 300, y: 150 },
+  ];
+
+  const svg = select('.canvas')
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 500);
+
+  svg.selectAll('circle')
+      .data(circles)
+      .enter()
+      .append('circle')
+      .attr('r', d => d.r)
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+      .attr('class', d => d.type);
+};
