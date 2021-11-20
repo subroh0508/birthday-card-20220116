b0b163bed5e83a56b1db55556ee61708d174938d
subroh0508 <subroh.0508@gmail.com>
2021-11-21 01:48:06 +0900

    [edit]Show gears

--- src/index.js
+++ src/index.js
@@ -1,23 +1,64 @@
-import { select } from 'd3-selection';
+import p5 from 'p5';
 
-window.onload = function () {
-  const circles = [
-    { type: 'small', r: 50, x: 100, y: 150 },
-    { type: 'medium', r: 100, x: 200, y: 150 },
-    { type: 'large', r: 150, x: 300, y: 150 },
-  ];
+const drawGear = (p, n, r) => {
+  let i = 0;
+  for(i = 0; i < p.TWO_PI; i += 0.01){
+    let y1 = 0;
+    let y2 = 0;
+    if(p.sin(i * n) >= 0){
+      y1 = r;
+    }else{
+      y1 = r * 1.1;
+    }
+    p.line(y1 * p.cos(i), y1 * p.sin(i), 0, 0);
+  }
+  p.ellipse(0, 0, 10);
+}
 
-  const svg = select('.canvas')
-      .append('svg')
-      .attr('width', 500)
-      .attr('height', 500);
+const sketch = (p) => {
+  p.preload = () => {
+  };
 
-  svg.selectAll('circle')
-      .data(circles)
-      .enter()
-      .append('circle')
-      .attr('r', d => d.r)
-      .attr('cx', d => d.x)
-      .attr('cy', d => d.y)
-      .attr('class', d => d.type);
+  p.setup = () => {
+    p.createCanvas(400, 400);
+    p.background(100);
+    p.frameRate(30);
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = () => {
+    p.background(100);
+    p.push();
+    p.stroke(p.color(200, 0, 0));
+    p.translate(200, 200);
+    p.rotate(p.frameCount / 10);
+    drawGear(p, 20, 50);
+    p.pop();
+
+    p.push();
+    p.stroke(p.color(0, 200, 0));
+    p.translate(92, 200);
+    p.rotate(-p.frameCount / 10 + 120);
+    drawGear(p, 20, 50);
+    p.pop();
+
+    p.push();
+    p.stroke(p.color(0, 0, 200));
+    p.translate(200, 92);
+    p.rotate(-p.frameCount / 10);
+    drawGear(p, 20, 50);
+    p.pop();
+
+    p.push();
+    p.stroke(p.color(0, 255, 255));
+    p.translate(277, 277);
+    p.rotate(-p.frameCount / 10 + 44);
+    drawGear(p, 20, 50);
+    p.pop();
+  }
 };
+
+new p5(sketch, document.body);
