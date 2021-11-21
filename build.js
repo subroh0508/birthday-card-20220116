3314c9f6961bb0038b9652f10ae7ca204d21f38f
subroh0508 <subroh.0508@gmail.com>
2021-11-21 14:54:24 +0900

    [edit]Enable auto reloading of browser for debug

--- build.js
+++ build.js
@@ -1,13 +1,15 @@
 const { build } = require('esbuild');
+const bs = require('browser-sync').create();
 
 const dev = process.argv.includes('--dev');
 const serve = process.argv.includes('--serve');
+const outdir = 'public';
 
 build({
   target: 'es2015',
   platform: 'browser',
   entryPoints: ['src/index.js'],
-  outfile: 'public/bundle.js',
+  outfile: `${outdir}/bundle.js`,
   bundle: true,
   minify: !dev,
   sourcemap: dev,
@@ -15,11 +17,28 @@ build({
     onRebuild(e, _) {
       if (e != null) {
         console.error('watch build failed:', e);
-      } else {
-        console.log('watch build succeeded!');
+        return
       }
+
+      console.log('watch build succeeded!');
     },
   } : false,
+}).then(() => {
+  if (serve) {
+    console.log('serving files...');
+    bs.init({
+      server: outdir,
+      watch: true,
+    }, (e) => {
+      if (e != null) {
+        console.error('serve failed:', e);
+        bs.exit();
+      }
+    })
+    return
+  }
+
+  console.log('build succeeded!');
 }).catch((e) => {
   console.error('build failed:', e);
   process.exit(1);
