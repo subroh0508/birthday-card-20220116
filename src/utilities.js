2faf15077532ae86619c63492fca99744b33684e
subroh0508 <subroh.0508@gmail.com>
2021-12-13 04:02:20 +0900

    [edit]Add distance method to utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,9 @@
+import { Circle } from './model/abstract/Circle';
+import { Clock } from './model/abstract/Clock';
+
 let id = 0;
+
+export const TWO_PI = Math.PI * 2;
 
 export function genUniqId() { return id++; }
 
@@ -21,3 +26,28 @@ export const combination = (objects, k) => {
 };
 
 export const degToRad = (degree) => Math.PI * 2 / 360 * degree;
+
+export const distance = (...args) => {
+  switch (args.length) {
+    case 1:
+      const model = args[0];
+      return this._distanceFromModel(model);
+    case 2:
+      const [x, y] = args;
+      return Math.sqrt(Math.pow(this.translateX - x, 2) + Math.pow(this.translateY - y, 2));
+    default:
+      return -1;
+  }
+};
+
+const _distanceFromModel = (model) => {
+  if (model instanceof Circle || model instanceof Clock) {
+    return this.distance(model.translateX, model.translateY);
+  }
+
+  if (model.hasOwnProperty('x') && model.hasOwnProperty('y')) {
+    return this.distance(model.x, model.y);
+  }
+
+  return -1;
+};
