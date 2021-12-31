b8292dfccfaa77e06efaef3b36be1e23a6bfb511
subroh0508 <subroh.0508@gmail.com>
2022-01-01 04:59:06 +0900

    [add]Add ClockCoverLayers

--- src/model/lantica/Yuika.js
+++ src/model/lantica/Yuika.js
@@ -1,18 +1,9 @@
+import { YUIKA_RADIUS } from './constants';
 import { Clock } from '../abstract/Clock';
 import { ClockHandleLayer } from './yuika/ClockHandleLayer';
 import { ClockDialLayer, ClockFaceLayer } from './yuika/ClockFaceLayers';
 import { ClockLongHandLayer, ClockShortHandLayer, ClockSecondHandLayer } from './yuika/ClockHandLayers';
-
-const YUIKA_RADIUS = 300;
-const YUIKA_COVER_HEIGHT = 120;
-const YUIKA_COVER_HINGE_WIDTH = 150;
-const YUIKA_COVER_THICKNESS = 30;
-const YUIKA_COVER_SURFACE_WIDTH = YUIKA_RADIUS * 2 - 20;
-const YUIKA_COVER_SURFACE_HEIGHT = 100;
-
-const YUIKA_CLOCK_COLOR_PRIMARY = '#EBA761';
-const YUIKA_CLOCK_COLOR_LIGHT = '#D8DFC5';
-const YUIKA_CLOCK_COLOR_DARK = '#5E3C2D';
+import { ClockCoverFrameLayer, ClockCoverHingeLayer, ClockCoverSurfaceLayer } from './yuika/ClockCoverLayers';
 
 export class Yuika extends Clock {
   constructor(p5, args) {
@@ -29,7 +20,7 @@ export class Yuika extends Clock {
     this.translate(this.translateX, this.translateY);
     this._drawHandle();
     this._drawFace();
-    //this._drawCover();
+    this._drawCover();
     this.pop();
   }
 
@@ -51,76 +42,19 @@ export class Yuika extends Clock {
   }
 
   _drawCover() {
-    const gFrame = this.createGraphics(YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT + 85);
+    this.translate(0, YUIKA_RADIUS + 55);
 
-    gFrame.translate(YUIKA_RADIUS, YUIKA_COVER_HEIGHT / 2 + 85);
-    gFrame.fill(YUIKA_CLOCK_COLOR_PRIMARY);
-    gFrame.stroke(YUIKA_CLOCK_COLOR_DARK);
-    gFrame.arc(0, 0, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT + 80, Math.PI, 0);
-    gFrame.ellipse(0, 0, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT);
-    gFrame.rect(-YUIKA_COVER_HINGE_WIDTH / 2, YUIKA_COVER_HEIGHT / 2 - 2, YUIKA_COVER_HINGE_WIDTH, 10);
-    gFrame.arc(0, -YUIKA_COVER_THICKNESS, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI / 60, Math.PI - Math.PI / 60);
-    gFrame.erase();
-    gFrame.arc(0, -YUIKA_COVER_THICKNESS - 1, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI / 60, Math.PI - Math.PI / 60);
-    gFrame.noErase();
-    gFrame.arc(0, 0, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI + Math.PI / 60, -Math.PI / 60);
-    gFrame.erase();
-    gFrame.arc(0, 1, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI + Math.PI / 60, -Math.PI / 60);
-    gFrame.noErase();
-    gFrame.erase();
-    gFrame.rect(-YUIKA_COVER_HINGE_WIDTH / 2, YUIKA_COVER_HEIGHT / 2 - 1, YUIKA_COVER_HINGE_WIDTH, 10);
-    gFrame.noErase();
-    gFrame.fill(YUIKA_CLOCK_COLOR_PRIMARY);
-    gFrame.stroke(YUIKA_CLOCK_COLOR_PRIMARY);
-    gFrame.rect(-YUIKA_COVER_HINGE_WIDTH / 2, 29, YUIKA_COVER_HINGE_WIDTH, 10);
-    gFrame.stroke(YUIKA_CLOCK_COLOR_DARK);
-    gFrame.arc(0, 32, YUIKA_COVER_HINGE_WIDTH, 20, Math.PI + Math.PI / 60, -Math.PI / 60);
-
-    const gSurface = this.createGraphics(YUIKA_COVER_SURFACE_WIDTH, YUIKA_COVER_SURFACE_HEIGHT);
-    gSurface.background(0);
-    gSurface.fill(YUIKA_CLOCK_COLOR_PRIMARY);
-    gSurface.stroke(YUIKA_CLOCK_COLOR_LIGHT);
-    [...Array(Math.floor(YUIKA_COVER_SURFACE_HEIGHT / 10) + 2)].forEach((_, i) => {
-      [...Array(Math.floor(YUIKA_COVER_SURFACE_WIDTH / 40) + 2)].forEach((_, j) => {
-        const [x, y] = [YUIKA_COVER_SURFACE_WIDTH - j * 40, YUIKA_COVER_SURFACE_HEIGHT - i * 10];
-        [...Array(2)].forEach((_, n) => {
-          gSurface.ellipse(x, y, 80 - n * 10, 20 - n * 10);
-        })
-      });
-    });
-    gSurface.erase();
-    gSurface.triangle(0, 0, 50, 0, 0, 40);
-    gSurface.triangle(0, YUIKA_COVER_SURFACE_HEIGHT, 65, YUIKA_COVER_SURFACE_HEIGHT, 0, YUIKA_COVER_SURFACE_HEIGHT - 30);
-    gSurface.triangle(YUIKA_COVER_SURFACE_WIDTH, YUIKA_COVER_SURFACE_HEIGHT, YUIKA_COVER_SURFACE_WIDTH - 65, YUIKA_COVER_SURFACE_HEIGHT, YUIKA_COVER_SURFACE_WIDTH, YUIKA_COVER_SURFACE_HEIGHT - 30);
-    gSurface.triangle(YUIKA_COVER_SURFACE_WIDTH, 0, YUIKA_COVER_SURFACE_WIDTH - 50, 0, YUIKA_COVER_SURFACE_WIDTH, 40);
-    gSurface.noErase();
-
-    this.translate(0, -YUIKA_RADIUS);
-    this.fill(0);
-    this.stroke(0);
-    this.rect(-25, 5, 50, 30);
-    this.fill(YUIKA_CLOCK_COLOR_LIGHT);
-    this.stroke(YUIKA_CLOCK_COLOR_LIGHT);
-    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 20, 25, YUIKA_COVER_HINGE_WIDTH / 2 - 35, 12);
-    this.rect(15, 25, YUIKA_COVER_HINGE_WIDTH / 2 - 35, 12);
-    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 20, 5, YUIKA_COVER_HINGE_WIDTH / 2 - 45, 20);
-    this.rect(25, 5, YUIKA_COVER_HINGE_WIDTH / 2 - 45, 20);
-    this.fill(YUIKA_CLOCK_COLOR_PRIMARY);
-    this.stroke(YUIKA_CLOCK_COLOR_PRIMARY);
-    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 21, 26, YUIKA_COVER_HINGE_WIDTH / 2 - 38, 10);
-    this.rect(17, 26, YUIKA_COVER_HINGE_WIDTH / 2 - 38, 10);
-    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 21, 6, YUIKA_COVER_HINGE_WIDTH / 2 - 48, 20);
-    this.rect(27, 6, YUIKA_COVER_HINGE_WIDTH / 2 - 48, 20);
-    this.stroke(YUIKA_CLOCK_COLOR_DARK);
-    this.rect(8, 0, 14, 20);
-    this.rect(-22, 0, 14, 20);
-    this.fill(YUIKA_CLOCK_COLOR_DARK);
-    this.rect(-6, 16, 12, 12);
-    this.ellipse(0, 14, 12);
-
-    this.translate(0, -YUIKA_COVER_THICKNESS - 20);
-    this.image(gSurface, -YUIKA_COVER_SURFACE_WIDTH / 2, -60);
-    this.image(gFrame, -YUIKA_RADIUS, -YUIKA_COVER_HEIGHT / 2 - 85);
+    const hingeLayer = new ClockCoverHingeLayer(this);
+    const frameLayer = new ClockCoverFrameLayer(this);
+    const surfaceLayer = new ClockCoverSurfaceLayer(this);
+    hingeLayer.setup();
+    frameLayer.setup();
+    surfaceLayer.setup();
+    this.translate(0, -hingeLayer.translateY);
+    this.image(hingeLayer, -hingeLayer.origin.x, -hingeLayer.origin.y);
+    this.translate(0, hingeLayer.translateY);
+    this.image(surfaceLayer, -surfaceLayer.origin.x, -surfaceLayer.origin.y);
+    this.image(frameLayer, -frameLayer.origin.x, -frameLayer.origin.y);
   }
 
   _drawHands() {
---
+++ src/model/lantica/yuika/ClockCoverLayers.js
@@ -0,0 +1,120 @@
+import compose from 'lodash/fp/compose';
+import { Layer } from '../../abstract/Layer';
+import { YUIKA_RADIUS } from '../constants';
+import { ClockTheme } from '../theme/ClockTheme';
+
+const ThemedLayer = compose(ClockTheme)(Layer);
+
+const YUIKA_COVER_HEIGHT = 120;
+const YUIKA_COVER_DEPTH = 40;
+const YUIKA_COVER_HINGE_HEIGHT = 40;
+const YUIKA_COVER_HINGE_WIDTH = 150;
+const YUIKA_COVER_THICKNESS = 30;
+const YUIKA_COVER_SURFACE_WIDTH = YUIKA_RADIUS * 2 - 20;
+const YUIKA_COVER_SURFACE_HEIGHT = 100;
+
+export class ClockCoverFrameLayer extends ThemedLayer {
+  constructor(p5, order) {
+    super(
+      p5,
+      { width: YUIKA_RADIUS * 2, height: YUIKA_COVER_HEIGHT + YUIKA_COVER_DEPTH },
+      { x: YUIKA_RADIUS, y: YUIKA_COVER_HEIGHT / 2 },
+      order,
+    );
+  }
+
+  draw() {
+    this.fill(this.primary);
+    this.stroke(this.dark);
+    this.arc(0, 0, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT + YUIKA_COVER_DEPTH * 2, 0, Math.PI);
+    this.arc(0, 0, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI - Math.PI / 60, Math.PI / 60);
+    this.arc(0, YUIKA_COVER_THICKNESS, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI + Math.PI / 60, -Math.PI / 60);
+    this.erase();
+    this.arc(0, YUIKA_COVER_THICKNESS + 1, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI + Math.PI / 60, -Math.PI / 60);
+    this.noErase();
+    this.arc(0, 0, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI / 60, Math.PI - Math.PI / 60);
+    this.erase();
+    this.arc(0, -1, YUIKA_RADIUS * 2, YUIKA_COVER_HEIGHT, Math.PI / 60, Math.PI - Math.PI / 60);
+    this.noErase();
+
+    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2, -YUIKA_COVER_HEIGHT / 2, YUIKA_COVER_HINGE_WIDTH, 2);
+    this.erase();
+    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2, -YUIKA_COVER_HEIGHT / 2 - 1, YUIKA_COVER_HINGE_WIDTH, 2);
+    this.noErase();
+    this.fill(this.primary);
+    this.stroke(this.primary);
+    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2, -39, YUIKA_COVER_HINGE_WIDTH, 10);
+    this.stroke(this.dark);
+    this.arc(0, -32, YUIKA_COVER_HINGE_WIDTH, 20, Math.PI / 60, Math.PI - Math.PI / 60);
+  }
+}
+
+export class ClockCoverHingeLayer extends ThemedLayer {
+  constructor(p5, order) {
+    super(
+      p5,
+      { width: YUIKA_COVER_HINGE_WIDTH, height: YUIKA_COVER_HINGE_HEIGHT },
+      { x: YUIKA_COVER_HINGE_WIDTH / 2, y: 0 },
+      order,
+    );
+  }
+
+  get translateY() { return YUIKA_COVER_HINGE_HEIGHT + YUIKA_COVER_HEIGHT / 2 - 4; }
+
+  draw() {
+    this.fill(0);
+    this.stroke(0);
+    this.rect(-25, 5, 50, 30);
+    this.fill(this.light);
+    this.stroke(this.light);
+    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 20, 5, YUIKA_COVER_HINGE_WIDTH / 2 - 35, 12);
+    this.rect(15, 5, YUIKA_COVER_HINGE_WIDTH / 2 - 35, 12);
+    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 20, 18, YUIKA_COVER_HINGE_WIDTH / 2 - 45, 20);
+    this.rect(25, 18, YUIKA_COVER_HINGE_WIDTH / 2 - 45, 20);
+    this.fill(this.primary);
+    this.stroke(this.primary);
+    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 21, 6, YUIKA_COVER_HINGE_WIDTH / 2 - 38, 10);
+    this.rect(17, 6, YUIKA_COVER_HINGE_WIDTH / 2 - 38, 10);
+    this.rect(-YUIKA_COVER_HINGE_WIDTH / 2 + 21, 16, YUIKA_COVER_HINGE_WIDTH / 2 - 48, 20);
+    this.rect(27, 16, YUIKA_COVER_HINGE_WIDTH / 2 - 48, 20);
+    this.stroke(this.dark);
+    this.rect(8, 20, 14, 20);
+    this.rect(-22, 20, 14, 20);
+    this.fill(this.dark);
+    this.rect(-6, 16, 12, 12);
+    this.ellipse(0, 28, 12);
+  }
+}
+
+export class ClockCoverSurfaceLayer extends ThemedLayer {
+  constructor(p5, order) {
+    super(
+      p5,
+      { width: YUIKA_COVER_SURFACE_WIDTH, height: YUIKA_COVER_SURFACE_HEIGHT },
+      { x: YUIKA_COVER_SURFACE_WIDTH / 2, y: YUIKA_COVER_SURFACE_HEIGHT / 2 - 10 },
+      order,
+    );
+  }
+
+  setup() { this.draw(); }
+
+  draw() {
+    this.background(0);
+    this.fill(this.primary);
+    this.stroke(this.light);
+    [...Array(Math.floor(YUIKA_COVER_SURFACE_HEIGHT / 10) + 2)].forEach((_, i) => {
+      [...Array(Math.floor(YUIKA_COVER_SURFACE_WIDTH / 40) + 2)].forEach((_, j) => {
+        const [x, y] = [YUIKA_COVER_SURFACE_WIDTH - j * 40, YUIKA_COVER_SURFACE_HEIGHT - i * 10];
+        [...Array(2)].forEach((_, n) => {
+          this.ellipse(x, y, 80 - n * 10, 20 - n * 10);
+        })
+      });
+    });
+    this.erase();
+    this.triangle(0, 0, 70, 0, 0, 40);
+    this.triangle(0, YUIKA_COVER_SURFACE_HEIGHT, 65, YUIKA_COVER_SURFACE_HEIGHT, 0, YUIKA_COVER_SURFACE_HEIGHT - 40);
+    this.triangle(YUIKA_COVER_SURFACE_WIDTH, YUIKA_COVER_SURFACE_HEIGHT, YUIKA_COVER_SURFACE_WIDTH - 65, YUIKA_COVER_SURFACE_HEIGHT, YUIKA_COVER_SURFACE_WIDTH, YUIKA_COVER_SURFACE_HEIGHT - 40);
+    this.triangle(YUIKA_COVER_SURFACE_WIDTH, 0, YUIKA_COVER_SURFACE_WIDTH - 70, 0, YUIKA_COVER_SURFACE_WIDTH, 40);
+    this.noErase();
+  }
+}
