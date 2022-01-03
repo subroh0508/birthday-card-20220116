24368b0aec0b54ee6ee097703281c0cc2c13e6e8
subroh0508 <subroh.0508@gmail.com>
2022-01-04 01:10:06 +0900

    [edit]Add designs of normal gear

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ const sketch = (p) => {
   const canvas = new Canvas(
     p,
     [
-      { radius: 50, teethCount: 20, color: '#00BB00', translate: { x: 600, y: 500 } },
-      { radius: 50, teethCount: 20, color: '#0000CC', translate: { x: 700, y: 500 } },
-      { radius: 50, teethCount: 20, color: '#001100', translate: { x: 500, y: 600 } },
-      { radius: 50, teethCount: 20, color: '#110000', translate: { x: 500, y: 700 } },
-      { radius: 50, teethCount: 20, color: '#000011', translate: { x: 600, y: 600 } },
+      { radius: 50, teethCount: 20, translate: { x: 600, y: 500 }, frameType: 1 },
+      { radius: 50, teethCount: 20, translate: { x: 700, y: 500 }, frameType: 2 },
+      { radius: 50, teethCount: 20, translate: { x: 500, y: 600 }, frameType: 3 },
+      { radius: 50, teethCount: 20, translate: { x: 500, y: 700 }, frameType: 4 },
+      { radius: 50, teethCount: 20, translate: { x: 600, y: 600 }, frameType: 5 },
+      { radius: 50, teethCount: 20, translate: { x: 600, y: 700 }, frameType: 6 },
     ],
     [
       { radius: 100, boneCount: 8, color: '#FFFFFF', translate: { x: 100, y: 400 }, direction: RotateDirection.LEFT },
--- src/model/lantica/LanticaGear.js
+++ src/model/lantica/LanticaGear.js
@@ -7,23 +7,21 @@ import {
 import compose from 'lodash/fp/compose';
 import { LanticaTheme } from './theme/LanticaTheme';
 import { GearLayer } from '../gear/GearLayer';
+import { TWO_PI } from '../../utilities';
 
 const ThemedGearLayer = compose(LanticaTheme)(GearLayer);
 
 export default class LanticaGear extends Gear {
+  _frameType = -1;
+
   constructor(p5, args) {
-    super(
-      p5,
-      {
-        ...args,
-        teethHeight: args.teethHeight || LANTICA_GEAR_TEETH_HEIGHT,
-        teethCount: LANTICA_GEAR_TEETH_COUNT,
-      }
-   );
+    super(p5, { ...args, ..._buildParams(args) });
 
     this._initRadian = LANTICA_INIT_RADIAN;
+    this._frameType = args.frameType || this._frameType;
   }
 
+  get frameType() { return this._frameType; }
   get gears() { return this.layers; };
   get clockHands() { return [] };
 
@@ -47,13 +45,174 @@ export default class LanticaGear extends Gear {
 
   buildLayers() {
     return [
-      new ThemedGearLayer(
+      new GearFrameLayer(
         this,
         this.radius,
         this.teethHeight,
         this.teethCount,
+        this.frameType,
         0,
       ),
     ];
+  }
+}
+
+const _buildParams = (args) => {
+  const defaultParams = {
+    teethHeight: args.teethHeight || LANTICA_GEAR_TEETH_HEIGHT,
+    teethCount: LANTICA_GEAR_TEETH_COUNT,
+  };
+
+  switch (args.frameType) {
+    case 1:
+    case 2:
+      return defaultParams;
+    case 3:
+      return { teethHeight: args.radius * 0.3, teethCount: 9 };
+    case 4:
+      return { teethHeight: args.radius * 0.15, teethCount: 5 };
+    case 5:
+    case 6:
+      return { ...defaultParams, teethHeight: args.radius * 0.2 };
+    default:
+      return defaultParams;
+  }
+}
+
+class GearFrameLayer extends ThemedGearLayer {
+  _type = -1;
+
+  constructor(p5, radius, teethHeight, teethCount, type, order) {
+    super(p5, radius, teethHeight, teethCount, order);
+
+    this._type = type;
+  }
+
+  get type() { return this._type; }
+
+  draw() {
+    const method = this[`_drawType${this.type}`];
+    if (typeof method === 'function') {
+      method.call(this);
+      return;
+    }
+
+    super.draw();
+  }
+
+  _drawType1() {
+    this.stroke(this.primary);
+    this.fill(this.primary);
+    this.frame(this.teethWidth, this.teethWidth - 2);
+
+    this.stroke(this.dark);
+    this.strokeWeight(2);
+    this.ellipse(0, 0, this.innerDiameter - 15);
+    this.ellipse(0, 0, this.innerDiameter - 35);
+  }
+
+  _drawType2() {
+    this.stroke(this.primary);
+    this.fill(this.primary);
+    this.frame(this.teethWidth, this.teethWidth - 2);
+
+    const patternDiameter = this.innerDiameter - 15;
+    this.stroke(this.dark);
+    this.ellipse(0, 0, patternDiameter);
+
+    const rad = TWO_PI / 3;
+    const diff = TWO_PI / 24;
+    [...Array(3)].forEach((_, i) => {
+      this.rotate(rad);
+      this.stroke(this.dark);
+      this.arc(-10, 0, patternDiameter + 20, patternDiameter, -diff / 2, diff / 2, this.PIE);
+      this.stroke(this.primary);
+      this.arc(-15, 0, 40, 40, -diff, diff, this.PIE);
+    });
+
+    this.stroke(this.dark);
+    this.ellipse(0, 0, 10);
+  }
+
+  _drawType3() {
+    this.stroke(this.primary);
+    this.fill(this.primary);
+    this.frame(this.teethWidth, this.teethWidth - 4);
+
+    this.ellipse(0, 0, this.innerDiameter + 2);
+    this.erase();
+    this.ellipse(0, 0, this.innerDiameter - 25);
+    this.noErase();
+  }
+
+  _drawType4() {
+    this.stroke(this.primary);
+    this.fill(this.primary);
+    this.frame(this.teethWidth / 2, this.teethWidth / 2 - 2);
+
+    this.stroke(this.dark);
+    this.ellipse(0, 0, this.innerDiameter);
+    this.ellipse(0, 0, this.innerDiameter - 20);
+  }
+
+  _drawType5() {
+    this.stroke(this.primary);
+    this.fill(this.primary);
+    this.frame(this.teethWidth, this.teethWidth - 2);
+
+    const patternDiameter = this.innerDiameter - 15;
+    this.stroke(this.dark);
+    this.ellipse(0, 0, patternDiameter);
+    this.erase();
+    this.ellipse(0, 0, patternDiameter - 2);
+    this.noErase();
+    this.stroke(this.primary);
+    this.ellipse(0, 0, patternDiameter - 30);
+
+    const rad = TWO_PI / 3;
+    const diff = TWO_PI / 24;
+    [...Array(3)].forEach((_, i) => {
+      this.rotate(rad);
+      this.stroke(this.dark);
+      this.arc(0, 0, patternDiameter, patternDiameter, -diff / 2, diff / 2, this.PIE);
+      this.rect(0, -3, patternDiameter / 2 - 11, 6);
+      this.stroke(this.primary);
+      this.rect(0, -2, patternDiameter / 2 - 10, 4);
+    });
+
+    this.stroke(this.dark);
+    this.ellipse(0, 0, 12);
+
+    [...Array(3)].forEach((_, i) => {
+      this.rotate(rad);
+      this.stroke(this.dark);
+      this.stroke(this.primary);
+      this.rect(4, -2, 5, 4);
+    });
+  }
+
+  _drawType6() {
+    this.stroke(this.primary);
+    this.fill(this.primary);
+    this.frame(this.teethWidth - 2, this.teethWidth - 4);
+
+    const patternDiameter = this.innerDiameter - 15;
+    this.stroke(this.dark);
+    this.ellipse(0, 0, patternDiameter);
+    this.ellipse(0, 0, patternDiameter - 40);
+
+    const rad = TWO_PI / 5;
+    const diff = TWO_PI / 24;
+    [...Array(5)].forEach((_, i) => {
+      this.rotate(rad);
+      this.stroke(this.dark);
+      this.arc(0, 0, patternDiameter, patternDiameter, -diff / 2, diff / 2, this.PIE);
+      this.rect(5, -3, patternDiameter / 2 - 16, 6);
+      this.stroke(this.primary);
+      this.rect(4, -2, patternDiameter / 2 - 14, 4);
+    });
+
+    this.stroke(this.primary);
+    this.ellipse(0, 0, 8);
   }
 }
